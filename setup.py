af91eb1bf27ba82467a4b4d7b0c4261bd1a85588
Saeed Mahmoodi <saeed.m201@gmail.com>
2021-06-01 21:34:46 +0430

    setup.py has been added.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open(join(dirname(__file__), 'todo.py')) as f:
+    version = re.match('.*__version__ = \'(.*?)\'', f.read(), re.S).group(1)
+
+DEPENDENCIES = [
+    'easycli',
+]
+
+
+
+setup(
+    name='todo',
+    version=version,
+    py_modules=['todo'],
+    description='make todo list via command line',
+    include_package_data=True,
+    install_requires=DEPENDENCIES,
+    license='MIT',
+    entry_points={
+        'console_scripts':[
+            'todo = todo:Todo.quickstart',
+        ]
+    }
+)
